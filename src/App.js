568f4ff207af25c398493e03d466e83b25799780
LeandroSimo <leo.bt.12@hotmail.com>
2022-09-16 22:46:54 -0300

    Creating Project Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
+// CSS
 import "./App.css";
+
+//React
+import { useCallback, useEffect, useState } from "react";
+
+//data
+import { wordsList } from "./data/words";
+
+//components
 import StartScreen from "./components/startscreen/StartScreen";
+import Game from "./components/game/Game";
+import GameOver from "./components/game_over/GameOver";
+
+const stages = [
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" },
+];
 
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name);
+
   return (
     <div className="App">
-      <StartScreen />
+      {gameStage === "start" && <StartScreen />}
+      {gameStage === "game" && <Game />}
+      {gameStage === "end" && <GameOver />}
     </div>
   );
 }
